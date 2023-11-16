536eedd005128231a4d33f916f9cf0d2a8c21ed8
AnnisaFadila069 <annisafd3r@gmail.com>
2023-11-16 15:32:10 +0700

    Pertemuan 16/11/2023

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -25,8 +25,18 @@ class DosenController extends Controller
     }
 
     public function showtime($jam){
-
     	return $jam;
+    }
 
+
+    public function formulir(){
+    	return view ('formulir');
+    }
+
+    public function proses(Request $request){
+    	$nama = $request->input('nama');
+        $alamat= $request->input('alamat');
+        $ipk= $request->input('ipk');
+        return "Nama: ".$nama."Alamat: ".$alamat.", IPK :".$ipk. ", isi asli: ".$request;
     }
 }
